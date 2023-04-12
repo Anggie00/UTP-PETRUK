e1f79508155456e9f5cd474416e8a33ebb0cf0fe
Anggie Armelia <112962745+Anggie00@users.noreply.github.com>
2023-04-12 22:35:03 +0700

    Add files via upload

---
+++ Cin ignore.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char nama[15];
+	char npm[10];
+	
+	cin.get(nama,15);
+	cin.ignore();
+	cin.get(npm,10);
+	
+	cout<< "Nama : "<< nama << endl;
+	cout<< "Npm : "<< npm << endl;
+	
+	return 0;
+}
