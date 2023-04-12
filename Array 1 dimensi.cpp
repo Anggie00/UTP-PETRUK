52d27edb0f857e1a5bbe04a082495b48b0fb2cf2
Anggie Armelia <112962745+Anggie00@users.noreply.github.com>
2023-04-12 21:51:45 +0700

    Add files via upload

---
+++ Array 1 dimensi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int myArray[5]= {10, 20, 30, 40, 50};
+	for(int i = 0; i < 5; i++){
+		cout<< " Value of elemet " << i << "is" << myArray[i] << endl;
+		
+	}
+	return 0;
+}
